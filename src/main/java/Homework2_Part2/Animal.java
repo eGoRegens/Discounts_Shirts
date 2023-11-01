84430e757ca6be86b45abf8d54b55e47b4ae750f
eGoRegens <125861111+eGoRegens@users.noreply.github.com>
2023-11-01 09:52:45 +0400

    перенос точки входа

---
+++ src/main/java/Homework2_Part2/Animal.java
@@ -0,0 +1,25 @@
+package Homework2_Part2;
+
+abstract class Animal {
+    public abstract void move();
+}
+class Cat extends Animal {
+    @Override
+    public void move() {
+        System.out.println("Я побежал");
+    }
+}
+
+class Fish extends Animal {
+    @Override
+    public void move() {
+        System.out.println("Я поплыла");
+    }
+}
+
+class Duck extends Animal {
+    @Override
+    public void move() {
+        System.out.println("Я полетела");
+    }
+}
